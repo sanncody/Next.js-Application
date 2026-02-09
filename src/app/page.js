3441807cece0019d4a4fc676c3b012d61602470c
Sandeep Wadhawan <sandeepwadhawan2001@gmail.com>
2026-02-10 01:09:03 +0530

    Fixed the basic scheme of tailwind in root page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    /*<div className="min-h-screen bg-zinc-800 flex items-center justify-center p-4 sm:p-6 md:p-8">
+    <div className="min-h-screen bg-zinc-800 flex items-center justify-center p-4 sm:p-6 md:p-8">
       <div className="max-w-2xl w-full bg-zinc-700 rounded-lg shadow-lg py-10 sm:py-12 md:py-16 px-6 sm:px-8 md:px-12">
         <div className="text-center px-3 sm:px-4">
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-amber-50 mb-4 sm:mb-6 leading-tight">
@@ -11,8 +11,6 @@ export default function Home() {
           </p>
         </div>
       </div>
-    </div>*/
-
-    <Clie
+    </div>
   );
 }
